9c69ef0a5dda4f6d08a07d8c5bb16817f995432a
mmt817 <1669404371@qq.com>
2024-09-11 16:36:23 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+  <el-button>1212</el-button>
+  <a-button danger>1212</a-button>
+  <button class="btn btn-info">Info</button>
+  <button class="btn btn-outline btn-info">Info</button>
+  <button class="btn btn-outline btn-success">Success</button>
+  <button class="btn btn-outline btn-warning">Warning</button>
+  <button class="btn btn-outline btn-error">Error</button>
+  <label class="swap swap-flip text-9xl">
+    <!-- this hidden checkbox controls the state -->
+    <input type="checkbox" />
+
+    <div class="swap-on">😈</div>
+    <div class="swap-off">😇</div>
+  </label>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,40 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+// 配置封装后 pinia
+import pinia from '@/stores'
+
+// 配置中文组件
+import ELementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+// 引入初始化全局样式
+// import '@/styles/common.scss'
+// 引入全局组件
+import { componentPlugin } from '@/components'
+// 引入懒加载指令
+import { lazyPlugin } from '@/directives'
+
+// ant-design-vue
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+// UnoCSS
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+// 挂载中文组件
+app.use(ELementPlus, {
+  locale: zhCn
+})
+app.use(Antd)
+// 挂载全局组件
+app.use(componentPlugin)
+// 注册自定义插件
+app.use(lazyPlugin)
+
+app.mount('#app')
